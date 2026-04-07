e61a098e95ccbb537deb5c5b28eff7b39413d2e6
livmikala <livgun01@iba.dk>
2026-04-07 16:07:15 +0200

    10 færdige spørgsmål i express quiz

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require("fs");
+const path = require("path");
 require('dotenv').config();
 
 
@@ -10,6 +12,12 @@ const PORT = process.env.PORT || 3000;
 app.use(cors()); 
 app.use(express.json())
 
+app.get("/quiz", (req, res) => {
+    const data = fs.readFileSync(path.join(__dirname,"quizzes", "express.json"), "utf-8");
+    const quiz = JSON.parse(data);
+
+    res.json(quiz);
+});
 
 app.listen(PORT, () => {
     console.log(`Serveren kører på http://localhost:${PORT}`);
