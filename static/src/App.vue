f8d238e8a71d9991833e236bdea3ae8a82c0b54a
chrcon01 <158613995+chrcon01@users.noreply.github.com>
2026-04-08 14:38:26 +0200

    Update App.vue
    opdateret med den rigtige kode

--- static/src/App.vue
+++ static/src/App.vue
@@ -1,11 +1,40 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <nav v-if="appStore.isLoggedIn()" class="top-nav">
+      <template v-if="appStore.isAdmin()">
+        <button @click="$router.push('/admin')">Upload quiz</button>
+        <button @click="$router.push('/admin')">Slet quiz</button>
+        <button @click="$router.push('/admin')">Se log</button>
+        <button @click="$router.push('/quiz')">Tag quiz</button>
+      </template>
+
+      <template v-else>
+        <button @click="$router.push('/quiz')">Tag test</button>
+        <button @click="$router.push('/history')">Se mine resultater</button>
+      </template>
+
+      <button class="logout-btn" @click="handleLogout">Log ud</button>
+    </nav>
+
+    <router-view />
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+import { appStore } from './stores/appStore'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      appStore
+    }
+  },
+  methods: {
+    handleLogout() {
+      appStore.logout()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
