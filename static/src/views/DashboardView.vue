4e9c89e0ffb78b963175cd349ca0500f5b6394b1
chrcon01 <158613995+chrcon01@users.noreply.github.com>
2026-04-08 14:31:31 +0200

    Create DashboardView.vue
    förste version

---
+++ static/src/views/DashboardView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <h1>Dashboard</h1>
+
+    <p>Logget ind som: {{ appStore.user?.username }}</p>
+    <p>Rolle: {{ appStore.user?.role }}</p>
+
+    <div class="dashboard-actions">
+      <template v-if="appStore.user?.role === 'admin'">
+        <button @click="$router.push('/admin')">Upload quiz</button>
+        <button @click="$router.push('/admin')">Slet quiz</button>
+        <button @click="$router.push('/admin')">Se log</button>
+        <button @click="$router.push('/quiz')">Tag quiz</button>
+      </template>
+
+      <template v-else>
+        <button @click="$router.push('/quiz')">Tag test</button>
+        <button @click="$router.push('/history')">Se mine resultater</button>
+      </template>
+
+      <button class="secondary-btn" @click="handleLogout">Log ud</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { appStore } from '../stores/appStore'
+
+export default {
+  name: 'DashboardView',
+  data() {
+    return {
+      appStore
+    }
+  },
+  methods: {
+    handleLogout() {
+      appStore.logout()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
